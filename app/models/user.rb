261620f6d5b70a2dceb30068c8eff2af6cab1881
othmane <othman-19@hotmail.fr>
2019-08-07 17:03:51 +0100

    add sign in page layout, add signin link , add the signin form,add a remember token to the users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  
+  before_create :create_remember_token
   validates :name, presence: true
   validates :email, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)     
+  end 
+
+  def User.new_token
+    Digest::SHA1.hexdigest SecureRandom.urlsafe_base64
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_154225) do
+ActiveRecord::Schema.define(version: 2019_08_07_124530) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_05_154225) do
     t.datetime "updated_at", null: false
     t.string "password_confirmation"
     t.string "password_digest"
+    t.string "remember_token"
   end
 
 end
