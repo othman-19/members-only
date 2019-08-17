1c39f7939b1ff09bc2dc0663295af6f458e39097
stickler-ci <support@stickler-ci.com>
2019-08-17 15:19:09 +0000

    Fixing style errors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "posts#index"
+  root 'posts#index'
   resources :users
   resources :posts, only: %i[new create index]
   get    '/login',   to: 'sessions#new'
