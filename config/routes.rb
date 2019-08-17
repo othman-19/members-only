dc2e1e9c2a847f9904a69165be67f1a7690660e7
othmane <othman-19@hotmail.fr>
2019-08-17 16:18:45 +0100

    Optional errors fixings after code review N°2: add root route, add sign up link to header, fix header css style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "posts#index"
   resources :users
   resources :posts, only: %i[new create index]
   get    '/login',   to: 'sessions#new'
